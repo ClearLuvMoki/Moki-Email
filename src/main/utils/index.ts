12bee3f540e6ec621a175a1a37b84906d5b0df70
Moki <moki@MokideMacBook-Pro.local>
2024-07-03 15:19:08 +0800

    remove: old file

--- src/main/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import {app} from "electron";
-
-export const isDev = !app.isPackaged;
-export const isDebug = process?.argv?.includes?.('--CTPDEV');
-export const isMac = process.platform === 'darwin';
-export const isLinux = process.platform === 'linux';
